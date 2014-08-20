512aefac98da87215231cdc5e751759c7effaa48
Matheus Andrade <matheus.amazonas@gmail.com>
2014-08-20 13:37:24 -0300

    Added XCode project files

---
+++ PICG-Project2/PICG-Project2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PICG-Project2
+//
+//  Created by Matheus Andrade on 8/20/14.
+//  Copyright (c) 2014 Matheus Andrade. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
