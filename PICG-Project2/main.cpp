54c4cf1ce937083f7d9cc6ea5aca965f64f10c5b
Matheus Andrade <matheus.amazonas@gmail.com>
2014-09-09 13:01:43 -0300

    Added Video reading/writing
    Added also a better sample video

--- PICG-Project2/main.cpp
+++ PICG-Project2/main.cpp
@@ -5,29 +5,68 @@
 //  Created by Matheus Andrade on 8/20/14.
 //  Copyright (c) 2014 Matheus Andrade. All rights reserved.
 //
+#include <iostream> // for standard I/O
+#include <string>   // for strings
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+using namespace cv;
+using namespace std;
 
-const char* sampleFileName = "sample1.avi";
+const char* inputFileName = "Wildlife.mp4";
+const char* outputFilePath = "/Users/matheusandrade/Desktop/output.mp4";
+VideoCapture inputVideo;
+VideoWriter outputVideo;
+
+
+void compare(Mat in, Mat out)
+{
+    imshow("Example-in", in);
+    imshow("Example-out", out);
+}
+
+Mat blur(Mat image)
+{
+    Mat out = image.clone();
+    GaussianBlur(image, out, Size(11,11), 0);
+    return out;
+}
+
 
 int main(int argc, const char * argv[])
 {
-    CvCapture *capture = cvCreateFileCapture(sampleFileName);
-    if (capture == NULL)
-    {
-        printf("Error while loading file %s\n", sampleFileName);
-    }
-    
+    Mat inFrame;
+    Mat outFrame;
+    cvNamedWindow("Derp Player");
+    inputVideo = VideoCapture(inputFileName);
+    Size frameSize = Size ((int) inputVideo.get(CV_CAP_PROP_FRAME_WIDTH),
+                           (int) inputVideo.get(CV_CAP_PROP_FRAME_HEIGHT));
+    int ex = CV_FOURCC('m', 'p', '4', 'v');
+
+    outputVideo.open(
+                     outputFilePath,
+                     ex,
+                     inputVideo.get(CV_CAP_PROP_FPS),
+                     frameSize,
+                     true);
+
     int c = 0;
-    while (cvGrabFrame(capture) != 0)
+    if (outputVideo.isOpened())
     {
-        c++;
+        while (c < 100)
+        {
+            if (!inputVideo.read(inFrame)) break;
+            outFrame = blur(inFrame);
+            //outputVideo.write(inFrame);
+            compare(inFrame, outFrame);
+            printf("Frame # %i\n", c);
+            c++;
+        }
     }
-    int frameCount = cvGetCaptureProperty(capture, 7);
-    printf("Frames reading the entire file: %i \nFrames accessing frameCount: %i\n", c, frameCount);
-    cvReleaseCapture(&capture);
-    
+    inputVideo.release();
+    outputVideo.release();
+    cvDestroyWindow("Derp Player");
     return 0;
 }
 
